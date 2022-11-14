6e8275b72077e25936792ab555b60c584278c096
Your Name <pjduri@gmail.com>
2022-11-14 11:13:42 -0600

    set up CD class

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,24 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc implements OpticalDisc {
 
     // TODO: Implement your custom interface.
 
+    public CD(String name, int storageCapacity, int remainingCapacity, int capacityUsed) {
+        super(name, storageCapacity, remainingCapacity, capacityUsed, "cd");
+    }
+
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+
+    @Override
+    public void spinDisc() {
+        System.out.println("A cd spins at a variable rate of 200 - 500 rpm.");
+    }
+
+    @Override
+    public void readData() {
+        System.out.println("Would you like to play a game?  (Jayde is funny).");
+    }
 
 }
